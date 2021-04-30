e3c1b2323a09298dece7a01c247f6fd56767c6c5
enti <gxmis791@gmail.com>
2021-04-30 11:21:29 +0200

    2021-4-30

---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/node
+
+const http = require("http");
+const node_static = require("node-static");
+
+const mongo = require("mongodb").MongoClient;
+
+let server_url = "mongodb://localhost:27017";
+
+let chat_db;
+
+mongo.connect(server_url, (err, server) => {
+	if (err){
+		console.log("Error en la conexión a MongoDB");
+		throw err;
+	}
+
+	console.log("Dentro de MongoDB");
+
+	chat_db = server.db("amongmeme");
+});
+
+
+
+console.log("Inicializando servidor chat");
+
+
+let public_files = new node_static.Server("pub");
+
+http.createServer( (request, response) => {
+	if (request.url == "/chat"){
+//		console.log("Entrando en el chat");
+		let cursor = chat_db.collection("chat").find({});
+
+		cursor.toArray().then( (data) => {
+			//console.log(data);
+
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+
+			response.write( JSON.stringify(data) );
+
+			response.end();
+		});
+
+		return;
+	}
+
+	if (request.url == "/submit"){
+		console.log("Envío de datos");
+
+		let body = [];
+		request.on('data', (chunk) => {
+
+			body.push(chunk);
+
+		}).on('end', () => {
+			
+			let chat_data = JSON.parse(Buffer.concat(body).toString());
+		
+			chat_db.collection("chat").insertOne({
+				user: chat_data.chat_user,
+				msg: chat_data.chat_msg,
+				date: Date.now()
+			});
+			 
+
+		});
+
+	
+
+		response.end();
+
+		return;
+	}
+
+	public_files.serve(request, response);
+
+}).listen(6891);
